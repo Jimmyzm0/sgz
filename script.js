022fec0213ffae0eacfdd40617737b4e7597590f
Jimmyzm0 <115449600+Jimmyzm0@users.noreply.github.com>
2023-12-12 14:55:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('play-music').addEventListener('click', function() {
-    // 添加音樂播放的代碼
-    alert('這裡可以播放音樂！');
+    var audio = new Audio('christmas-music.mp3'); // 替換成您的音樂文件路徑
+    audio.play();
 });
