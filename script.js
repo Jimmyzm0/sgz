b232839a5fe3c7dd466820963c53b71b1b0d95c8
Jimmyzm0 <115449600+Jimmyzm0@users.noreply.github.com>
2023-12-12 15:29:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById('play-music').addEventListener('click', function() {
-    var audio = new Audio('christmas-music.mp3'); // 替換成您的音樂文件路徑
-    audio.play();
+const blessings = [
+    "祝您聖誕快樂，新年進步！",
+    "願您的聖誕充滿溫馨、快樂、和平！",
+    "希望聖誕老人給您帶來滿滿的幸福和笑容！",
+    "在這個美好的節日裏，願您的每一天都閃耀著快樂的光芒！"
+    // 您可以根據需要添加更多祝福
+];
+
+function getRandomBlessing() {
+    const randomIndex = Math.floor(Math.random() * blessings.length);
+    return blessings[randomIndex];
+}
+
+document.getElementById('blessingButton').addEventListener('click', function() {
+    const blessing = getRandomBlessing();
+    document.getElementById('blessingText').textContent = blessing;
 });
